128f9eae0d11b87c470f3fcf56d3799867eb5cf2
Dominick Grift <dac.override@gmail.com>
2015-02-23 09:04:23 +0100

    if there is a EXCLUDE.local file with something in it then that will be used to exclude .cil suffixed files if they exist.

--- laptop.sh
+++ laptop.sh
@@ -5,17 +5,23 @@ VERSION=29
 
 test -d policy || /bin/mkdir -m 0700 policy
 
-EXCLUDE="tests"
+EXCLUDE_LOCAL="EXCLUDE.local"
+
+if [ -e "${PWD}/${EXCLUDE_LOCAL}" -a -s "${PWD}/${EXCLUDE_LOCAL}" ] ; then
+    EXCLUDE+=($(/bin/cat "${PWD}/${EXCLUDE_LOCAL}"))
+else
+    EXCLUDE="test"
+fi
 
 gen_listing () {
     local MODULES=$(/bin/find . -type f \( -iname "*.cil" \) | /bin/cut -d/ -f2-)
 
-    for i in ${EXCLUDE} ;
+    for i in ${EXCLUDE[*]} ;
         do
-            MODULES=$(printf "%s\n" "${MODULES}" | /bin/grep -v $i)
+            MODULES=$(printf "%s\n" "${MODULES[*]}" | /bin/grep -v $i)
         done
 
-    printf "%s\n" "${MODULES}" | /bin/sort > LISTING
+    printf "%s\n" "${MODULES[*]}" | /bin/sort > LISTING
 }
 
 ${SECILC} -v \
